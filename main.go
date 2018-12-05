daaf978d0b0e9d512c6d51e1cf39552c1b74d2ca
leeningli <1507045477@qq.com>
2018-12-05 18:48:26 +0800

    use govender

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+
+	"github.com/buaazp/fasthttprouter"
+	"github.com/valyala/fasthttp"
 )
+
+type TestResp struct {
+	Name string `json:"name"`
+	Desc string `json:"desc"`
+}
 
 func main() {
 	fmt.Println("Hello, leeingli.")
+	router := fasthttprouter.New()
+	router.GET("/", testFunc)
+	if err := fasthttp.ListenAndServe("0.0.0.0:12345", router.Handler); err != nil {
+		fmt.Println("start fasthttp fail:", err.Error())
+	}
+}
+
+func testFunc(ctx *fasthttp.RequestCtx) {
+	ctx.SetContentType("application/json")
+	ctx.SetStatusCode(200)
+	data := TestResp{
+		Name: "leeingli",
+		Desc: "this is test for github&&wercker&&dockerhub",
+	}
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("struct to json is failed:", err)
+	}
+	ctx.WriteString(string(dataBytes))
 }
